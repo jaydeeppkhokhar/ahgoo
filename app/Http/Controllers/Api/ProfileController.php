03ff814dfbd531bec83dc041b8257ef1f7ba38a1
Sudip Karmakar <developer.muktiprime@gmail.com>
2024-07-26 14:00:13 +0530

    Profile image modify

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -784,18 +784,18 @@ class ProfileController extends Controller
 
         try {
             $updated_data = array();
-            $updated_data['step'] = $request->step
+            $updated_data['step'] = $request->step;
             $profilePicPath = null;
             if ($request->hasFile('profile_pic')) {
                 $profilePicPath = $request->file('profile_pic')->store('profile_pics', 'public');
                 $profilePicUrl = Storage::url($profilePicPath);
-                $updated_data['profile_pic'] => 'http://34.207.97.193/ahgoo/public'.$profilePicUrl,
+                $updated_data['profile_pic'] => 'http://34.207.97.193/ahgoo/public'.$profilePicUrl;
             }
             $coverPicPath = null;
             if ($request->hasFile('cover_pic')) {
                 $coverPicPath = $request->file('cover_pic')->store('cover_pic', 'public');
                 $coverPicUrl = Storage::url($coverPicPath);
-                $updated_data['cover_pic'] => 'http://34.207.97.193/ahgoo/public'.$coverPicUrl,
+                $updated_data['cover_pic'] => 'http://34.207.97.193/ahgoo/public'.$coverPicUrl;
             }
             AllUser::where('_id', $request->user_id)->update($updated_data);
 
